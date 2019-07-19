2ca5c886b9e231d5879d44768639edd09adad88e
Aniketh <anikethnair97@gmail.com>
2019-07-19 22:46:22 +0530

    Menu structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
-
+import Menu from './components/MenuComponent';
 function App() {
   return (
     <div className="App">
@@ -11,6 +11,7 @@ function App() {
           <NavbarBrand href="/">Reactstrap</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
