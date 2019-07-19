3ca12228fa573b4906f3d8553063c4d1d917e7aa
Aniketh <anikethnair97@gmail.com>
2019-07-19 21:53:33 +0530

    Navbar added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/boostrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
