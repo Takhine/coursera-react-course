062623cb67e9661a942867f404bf24f6501c5cf2
Aniketh <anikethnair97@gmail.com>
2019-07-20 10:35:13 +0530

    Assignment 1
    New component Dishdetailcomponent created

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class DishdetailComponent extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+
+        );
+    }
+}
+
+export default DishdetailComponent;
