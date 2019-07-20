11ab36210ccc481c54ffe325715f312cdb4703c0
Aniketh <anikethnair97@gmail.com>
2019-07-20 09:03:43 +0530

    Lifted state
    lifted state to app.js and used props instead of state in Menu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Reactstrap</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+import {DISHES} from './shared/dishes';
+class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      dishes:DISHES
+    };
+  }
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Reactstrap</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
