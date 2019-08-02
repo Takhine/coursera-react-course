6e104814cddca6be6e57ff580afdf5fd873f6298
Aniketh <anikethnair97@gmail.com>
2019-08-02 15:30:53 +0530

    Styling

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 class Main extends Component {
 
   constructor(props) {
@@ -21,6 +22,7 @@ class Main extends Component {
         <Header/>
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer/>
       </div>
     );
   }
