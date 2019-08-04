a45478f2b7f4320854679e3e07a4ea3c62070fd7
Aniketh <anikethnair97@gmail.com>
2019-08-04 16:46:48 +0530

    breadcrumb added

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
-
+import {Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 	function RenderMenuItem({dish, onClick}){
 		return(
 			<Card key={dish.id}
-                onClick={() => onClick(dish.id)}>
+                >
+					<Link to={`/menu/${dish.id}`}>
 					<CardImg width="100%" src={dish.image} alt={dish.name}/>							
 					<CardImgOverlay className="ml-5">
 					<CardTitle>{dish.name}</CardTitle>
 				</CardImgOverlay>
+				</Link>
 			</Card>
 		);
 	}
@@ -20,12 +22,24 @@ import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 					key={dish.id}
 					className="col-12 col-md-5 m-1"			
 				>
-				<RenderMenuItem dish={dish} onClick={props.onClick}/>					 
+				<RenderMenuItem dish={dish}/>					 
 				</div>
 				);
 		});
 		return(
 			<div className="container">
+				<div className="row">
+					<Breadcrumb>
+						<BreadcrumbItem>
+							<Link to='/home'>Home</Link>
+						</BreadcrumbItem>
+						<BreadcrumbItem active>Menu</BreadcrumbItem>
+					</Breadcrumb>
+					<div className="col-12">
+					<h3>Menu</h3>
+					<hr/>
+					</div>
+				</div>
 				<div className="row">
 						{menu}
 				</div>
