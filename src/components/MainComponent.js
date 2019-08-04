9e298402ee27edd1a5efa563eeab14fd608d24ee
Aniketh <anikethnair97@gmail.com>
2019-08-04 16:31:23 +0530

    Home component done

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,9 @@ import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {PROMOTIONS} from '../shared/promotions';
+import {LEADERS} from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
@@ -13,6 +16,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
+        comments:COMMENTS,
+        promotions:PROMOTIONS,
+        leaders:LEADERS,
         selectedDish: null
     };
   }
@@ -22,7 +28,11 @@ class Main extends Component {
   render() {
     const HomePage=()=>{
       return(
-        <Home/>
+      <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
+      promotion={this.state.promotions.filter((promo)=>promo.featured)[0]}
+      leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+      
+      />
       );
     }
     return (
