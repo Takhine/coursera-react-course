8a0cfb5c9d511acb89656d96b5e0ec5e399dbf7e
Aniketh <anikethnair97@gmail.com>
2019-08-04 17:01:21 +0530

    contact component  finished

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle,Breadcrumb, BreadcrumbItem} from 'reactstrap';
 import moment from 'moment';
+import {Link} from 'react-router-dom';
     function RenderDish({dish}){
         return(
 		<Card>
@@ -42,14 +43,26 @@ import moment from 'moment';
             if(props.dish !=null){
                 return(
                     <div className="container">
-                    <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
+                        <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to='/menu'>Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr/>
+                        </div>
                     </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.dish.comments} />
-                    </div>
-                    </div>
+                        <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={props.comments} />
+                        </div>
+                        </div>
                 </div>
                 );
             }
